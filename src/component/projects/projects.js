af54a66fa497471aa750ab3243db3d0989a1ad36
ashabrai frauen <ashabraifrauen@yahoo.com>
2020-08-11 09:55:49 -0700

    updating project

--- src/component/projects/projects.js
+++ src/component/projects/projects.js
@@ -12,17 +12,17 @@ class Projects extends React.Component {
                 </div>
 
                 <div className='projectDiv'>
+                    <div><h3>COVID-19</h3>
+                        <p>A data app, that allows a user to see COVID-19 data results on ether a state or country.</p>
+                        <a href='http://cov19-reactapp.herokuapp.com/'>Click to view site</a>
+                    </div>
                     <div><h3>Change Healthcare/Trading Partner</h3>
-                    <p>Developed a public-facing application allowing end-users to search for Healthcare providers/payers.</p>
-                    <a href='https://payerlist.changehealthcare.com/'>Click to view site</a>
+                        <p>Developed a public-facing application allowing end-users to search for Healthcare providers/payers.</p>
+                        <a href='https://payerlist.changehealthcare.com/'>Click to view site</a>
                     </div>
                     <div><h3>Website</h3>
-                    <p>A ongoing project, constantly updating with new work and information that I've done. Currently you are viewing the site.</p>
-                    <a href='https://github.com/ashabrai/PersonalWebsite'>Click to view Github Repo</a>
-                    </div>
-                    <div><h3>MOOD FELLOWS</h3>
-                    <p>A complete app built with a team, has functional front-end and back-end logic. A blog forum that allows you to post anonomously. App judges the context of the text.</p>
-                    <a href='http://www.moodfellows.org/'>Click to view site</a>
+                        <p>A ongoing project, constantly updating with new work and information that I've done. Currently you are viewing the site.</p>
+                        <a href='https://github.com/ashabrai/PersonalWebsite'>Click to view Github Repo</a>
                     </div>
                 </div>
 
