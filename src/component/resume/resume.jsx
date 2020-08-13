5f0c437c52e9c964d75893a6a1d87acba5665cfe
ashabrai frauen <ashabraifrauen@yahoo.com>
2020-08-12 19:52:05 -0700

    hopefully this fixes issue

--- src/component/resume/resume.jsx
+++ src/component/resume/resume.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Iframe from 'react-iframe';
 import './resume.scss';
 
@@ -6,16 +6,16 @@ export default class resume extends Component {
     render() {
         return (
             <div className='wrapper'>
-            <div className="resume">
-                <Iframe url="https://docs.google.com/document/d/e/2PACX-1vTe4d5QCXzAnXhwKFuk7Vzn4K0Hc45XF8fa_BDQM1WUenXAEHeI5eb1VINJ-Jhj8OzzPURI-RQ5O6_I/pub?embedded=true"
-                    width="60%"
-                    height="900px"
-                    className="resume"
-                    display="initial"
+                <div className="resume">
+                    <Iframe url="https://docs.google.com/document/d/e/2PACX-1vTe4d5QCXzAnXhwKFuk7Vzn4K0Hc45XF8fa_BDQM1WUenXAEHeI5eb1VINJ-Jhj8OzzPURI-RQ5O6_I/pub?embedded=true"
+                        width="60%"
+                        height="900px"
+                        className="resume"
+                        display="initial"
                     />
-            </div>                     
-            <div className="message"><p>Please view on desktop</p></div>
-</div>
+                </div>                     
+                <div className="message"><p>Please view on desktop</p></div>
+            </div>
            
         )
     }
